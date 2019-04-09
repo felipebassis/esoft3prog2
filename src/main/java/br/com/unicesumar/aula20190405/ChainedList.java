7cc48ef834e4342f8556dbffaadeddda8b69e684
Felipe Bezerra de Assis <39170581+felipebassis@users.noreply.github.com>
2019-04-09 19:34:18 -0300

    Finalizada implementacao da lista encadeada.

--- src/main/java/br/com/unicesumar/aula20190405/ChainedList.java
+++ src/main/java/br/com/unicesumar/aula20190405/ChainedList.java
@@ -1,6 +1,7 @@
 package br.com.unicesumar.aula20190405;
 
 import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 public class ChainedList<T> {
     private Element<T> first;
@@ -10,14 +11,13 @@ public class ChainedList<T> {
     }
 
     public void add(T element) {
-        if (isNull(first.value)){
+        if (isNull(first.value)) {
             this.first = new Element<>(element);
-        }
-        else {
+        } else {
             Element<T> temporary = first;
             boolean isNotAdded = true;
-            while (isNotAdded){
-                if (isNull(temporary.nextValue)){
+            while (isNotAdded) {
+                if (isNull(temporary.nextValue)) {
                     temporary.nextValue = new Element<>(element);
                     isNotAdded = false;
                 } else {
@@ -25,6 +25,40 @@ public class ChainedList<T> {
                 }
             }
         }
+    }
+
+    public void remove(T element) {
+        if (first.value == element) {
+            first = first.nextValue;
+        } else {
+            searchAndRemove(first, first.nextValue, element);
+        }
+    }
+
+    private void searchAndRemove(Element<T> previousValue, Element<T> nextValue, T element) {
+        if (nextValue.value == element) {
+            previousValue.nextValue = nextValue.nextValue;
+        } else {
+            if (isNull(nextValue.nextValue)) {
+                throw new RuntimeException("Element " + element + " not found");
+            }
+            searchAndRemove(previousValue.nextValue, nextValue.nextValue, element);
+        }
+    }
+
+    public T get(int index) {
+        Element<T> element = first;
+        try {
+            for (int iterator = 0; iterator < index; iterator++) {
+                element = element.nextValue;
+            }
+        } catch (Exception e) {
+            throw new ArrayIndexOutOfBoundsException("Index " + index + " is null.");
+        }
+        if (nonNull(element)) {
+            return element.value;
+        }
+        throw new ArrayIndexOutOfBoundsException("Index " + index + " is null.");
     }
 
     private static class Element<T> {
--- src/test/java/br/com/unicesumar/aula20190405/ChainedListTest.java
+++ src/test/java/br/com/unicesumar/aula20190405/ChainedListTest.java
@@ -1,6 +1,7 @@
 package br.com.unicesumar.aula20190405;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,9 +10,18 @@ class ChainedListTest {
     @Test
     void add() {
         ChainedList<String> chainedList = new ChainedList<>("Test");
-        chainedList.add("Test1");
+        assertEquals("Test", chainedList.get(0));
 
+        chainedList.add("Test2");
+        assertEquals("Test2", chainedList.get(1));
 
+        chainedList.remove("Test");
+        assertEquals("Test2", chainedList.get(0));
 
+        Executable outOfBounds = () -> chainedList.get(10);
+        assertThrows(ArrayIndexOutOfBoundsException.class, outOfBounds);
+
+        Executable removeException = () -> chainedList.remove("Teste");
+        assertThrows(RuntimeException.class, removeException);
     }
 }
